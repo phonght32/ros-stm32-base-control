5ce30d46fae2f017be6d6ef43e233bc58f08b953
phonght32 <thanhphongho1998@gmail.com>
2024-01-11 23:31:51 +0700

    initialize timer3 PD2 as ETR

--- Core/Inc/tim.h
+++ Core/Inc/tim.h
@@ -28,14 +28,14 @@ extern "C" {
 #include "main.h"
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
-extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
 extern TIM_HandleTypeDef htim13;
 extern TIM_HandleTypeDef htim14;
 /* USER CODE BEGIN Private defines */
 /* USER CODE END Private defines */
-void MX_TIM1_Init(void);
 void MX_TIM2_Init(void);
+void MX_TIM3_Init(void);
 void MX_TIM13_Init(void);
 void MX_TIM14_Init(void);
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
